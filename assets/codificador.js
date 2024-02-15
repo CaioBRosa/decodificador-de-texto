5b6a435cf55fb2f9efc3c9e960e710a3551215ea
Caio B. Rosa <caiobastos.rosa670@hotmail.com>
2024-02-15 12:02:25 -0300

    Validacoes feitas

--- assets/codificador.js
+++ assets/codificador.js
@@ -7,30 +7,53 @@
 //A letra "o" é convertida para "ober"
 //A letra "u" é convertida para "ufat"
 
+window.onload = function () {
+    let textarea1 = document.getElementById("input1")
+    let textarea2 = document.getElementById("input2")
+    textarea1.value = ""
+    textarea2.value = ""
+}
+
+function validarNumero(valor) {
+    return isNaN(valor)
+}
+
+function validarUTF8(valor) {
+    const regex = /[^\x00-\x7F]/g;
+    return !regex.test(valor)
+}
+
 function obterMensagem() {
     let mensagem = document.getElementById("input1").value
     let codificado = document.getElementById('input2')
 
     if (mensagem == "") {
-        codificado.value = "Insira um texto para ser codificado!"
+        codificado.value = "Insira uma mensagem de texto para ser codificado!"
     } else {
-
-        const codigo = {
-            e: "enter",
-            i: "imes",
-            a: "ai",
-            o: "ober",
-            u: "ufat",
+        if (!validarNumero(mensagem)) {
+            codificado.value = "Insira uma mensagem de texto para ser codificado!"
+        } else {
+            if (!validarUTF8(mensagem)) {
+                codificado.value = "Insira uma mensagem de texto para ser codificado!"
+            } else {
+                const codigo = {
+                    e: "enter",
+                    i: "imes",
+                    a: "ai",
+                    o: "ober",
+                    u: "ufat",
+                }
+    
+                let mensagemCodificada = ""
+    
+                for (let i = 0; i < mensagem.length; i++) {
+                    const currentChar = mensagem.charAt(i).toLowerCase()
+                    mensagemCodificada += codigo[currentChar] || currentChar
+                }
+    
+                codificado.value = mensagemCodificada
+            }
         }
-
-        let mensagemCodificada = ""
-
-        for (let i = 0; i < mensagem.length; i++) {
-            const currentChar = mensagem.charAt(i).toLowerCase()
-            mensagemCodificada += codigo[currentChar] || currentChar
-        }
-
-        codificado.value = mensagemCodificada
     }
 }
 
@@ -40,50 +63,47 @@ function decodificar() {
     let decodificado = document.getElementById('input2')
 
     if (mensagemCodificada == "") {
-        decodificado.value = "Insira um texto para ser decodificado!"
+        decodificado.value = "Insira uma mensagem de texto para ser decodificado!"
     } else {
 
-        const codigoInvertido = {
-            "ai": "a",
-            "ober": "o",
-            "imes": "i",
-            "ufat": "u",
-            "enter": "e",
-        };
-
-        let mensagemDecodificada = ""
-        let i = 0;
-
-        while (i < mensagemCodificada.length) {
-            const currentChar = mensagemCodificada.charAt(i).toLowerCase();
-            let foundMatch = false;
-
-            for (const key in codigoInvertido) {
-                if (key.charAt(0) === currentChar) {
-                    mensagemDecodificada += codigoInvertido[key];
-                    i += key.length;
-                    foundMatch = true;
-                    break;
+        if (!validarNumero(mensagemCodificada)) {
+            decodificado.value = "Insira uma mensagem de texto para ser decodificado!"
+        } else {
+            if (!validarUTF8(mensagemCodificada)) {
+                decodificado.value = "Insira uma mensagem de texto para ser decodificado!"
+            } else {
+                const codigoInvertido = {
+                    "ai": "a",
+                    "ober": "o",
+                    "imes": "i",
+                    "ufat": "u",
+                    "enter": "e",
+                };
+    
+                let mensagemDecodificada = ""
+                let i = 0;
+    
+                while (i < mensagemCodificada.length) {
+                    const currentChar = mensagemCodificada.charAt(i).toLowerCase();
+                    let foundMatch = false;
+    
+                    for (const key in codigoInvertido) {
+                        if (key.charAt(0) === currentChar) {
+                            mensagemDecodificada += codigoInvertido[key];
+                            i += key.length;
+                            foundMatch = true;
+                            break;
+                        }
+                    }
+    
+                    if (!foundMatch) {
+                        mensagemDecodificada += currentChar;
+                        i++;
+                    }
                 }
-            }
-
-            if (!foundMatch) {
-                mensagemDecodificada += currentChar;
-                i++;
+    
+                decodificado.value = mensagemDecodificada
             }
         }
-
-        decodificado.value = mensagemDecodificada
     }
 }
-
-
-window.onload = function () {
-    let textarea1 = document.getElementById("input1")
-    let textarea2 = document.getElementById("input2")
-    textarea1.value = ""
-    textarea2.value = ""
-};
-
-
-
